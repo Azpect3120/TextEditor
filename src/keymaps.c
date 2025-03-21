dfd6b2ee402159899111f54d72f02b3cb8973b6b
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-03-20 23:49:56 -0700

    CLEAN: Deleted and added some comments.

--- src/keymaps.c
+++ src/keymaps.c
@@ -12,9 +12,9 @@ KeyMap normal_mode_keymaps[] = {
     {'A', action_insert_mode_append_end},
     {'o', action_insert_mode_below},
     {'O', action_insert_mode_above},
-    {'w', action_move_next_word_start},
-    {'b', action_move_prev_word_start},
-    {'e', action_move_curr_word_end},
+    {'w', action_move_next_word_start}, // TODO: differentiate W and w
+    {'b', action_move_prev_word_start}, // TODO: differentiate B and b
+    {'e', action_move_curr_word_end},   // TODO: differentiate E and e
     {'0', action_move_start_line},
     {'$', action_move_end_line},
     {'_', action_move_first_char},
@@ -55,8 +55,6 @@ KeyMap insert_mode_keymaps[] = {
 void editor_process_key_press(Editor *E, const int c) {
 
     // editor_set_status_message(E, "Key pressed: '%c' (%d)", (c == '\n') ? ' ' : c, c);
-
-
     switch (E->mode) {
         case NORMAL_MODE:
             execute_command_normal(E, c);
@@ -67,47 +65,6 @@ void editor_process_key_press(Editor *E, const int c) {
         case COMMAND_MODE:
             break;
     }
-
-    return;
-
-    // Ctrl-H is caught here
-    if (c == KEY_BACKSPACE) {
-        editor_remove_character(E, E->cur_x, E->cur_y);
-    } else if (c == KEY_DOWN) {
-        action_move_cursor(E, DIRECTION_DOWN);
-    } else if (c == KEY_UP) {
-        action_move_cursor(E, DIRECTION_UP);
-    } else if (c == KEY_LEFT) {
-        action_move_cursor(E, DIRECTION_LEFT);
-    } else if (c == KEY_RIGHT) {
-        action_move_cursor(E, DIRECTION_RIGHT);
-    // Ctrl-M and Ctrl-J are caught here
-    } else if (c == '\n' || c == KEY_ENTER || c == '\r') {
-        editor_insert_newline(E);
-    // Keys between 1 and 26 SHOULD be control-characters
-    } else if (iscntrl(c) && c >= 1 && c <= 26) {
-        // Ctrl-C or Ctrl-Q: Quit
-        if (c == 3 ||c == 17) return;
-
-        // Ctrl-S: Save
-        if (c == 19) {
-            editor_save_file(E);
-            return;
-        }
-
-        // Catch Ctrl-I as tab for some reason...
-        if (c == 9) {
-            editor_insert_character(E, E->cur_x, E->cur_y, (char) c);
-            return;
-        }
-
-        // TODO: Fix this with renders, right now, they are 2 characters
-        // editorInsertCharacter(E->cur_x, E->cur_y, '^');
-        editor_insert_character(E, E->cur_x, E->cur_y, (char) (c | 0x40)); // Set the 6th bit (0x40 = 64)
-    } else {
-        editor_insert_character(E, E->cur_x, E->cur_y, (char) c);
-    }
-    return;
 }
 
 int execute_command_normal(Editor *E, const int command) {
